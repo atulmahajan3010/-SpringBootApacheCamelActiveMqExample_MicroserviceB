43efebceb15514218b8dbe04e1d1e4fc10d7b066
atulmahajan3010 <atulmahajan301090@gmail.com>
2021-08-13 15:09:04 +0530

    Micriservice B

---
+++ src/main/java/com/apacheCamel/SpringBootApacheCamel02B/SpringBootApacheCamel02BApplication.java
@@ -0,0 +1,13 @@
+package com.apacheCamel.SpringBootApacheCamel02B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApacheCamel02BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApacheCamel02BApplication.class, args);
+	}
+
+}
